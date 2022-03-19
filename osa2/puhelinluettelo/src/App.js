096786968f62fe89a1299c5cdc90a5a19ff95e4a
Topi Kanninen <topi.kanninen@sci.fi>
2022-03-19 22:44:32 +0200

    puhelinluettelo 2.7

--- osa2/puhelinluettelo/src/App.js
+++ osa2/puhelinluettelo/src/App.js
@@ -1,17 +1,46 @@
 import { useState } from 'react'
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas' }
-  ]) 
+  const [persons, setPersons] = useState([]) 
   const [newName, setNewName] = useState('')
+
+
+  const handleNameChange = (event) => {
+    setNewName(event.target.value)
+  }
+
+  const addPerson = (event) => {
+    event.preventDefault()
+    const nameObject = {
+      name: newName,
+    }
+    console.log(nameObject.name)
+    console.log(persons.includes(nameObject.name))
+
+    if (persons.some(person => person['name'] === newName) === true) {
+      window.alert(nameObject.name + " is already added to phonebook")
+    } else {
+      setPersons(persons.concat(nameObject))
+      setNewName('')
+    }
+
+  }
+
+  const Person = ({ person }) => {
+    return (
+      <p>{person.name}</p>
+    )
+  }
 
   return (
     <div>
       <h2>Phonebook</h2>
-      <form>
+      <form onSubmit={addPerson}>
         <div>
-          name: <input />
+          name: <input
+            value={newName}
+            onChange={handleNameChange}
+          />
         </div>
         <div>
           <button type="submit">add</button>
@@ -19,11 +48,10 @@ const App = () => {
       </form>
       <h2>Numbers</h2>
         {persons.map(person =>
-          <p key={person.name} person={person} />
-        )}
+          <Person key={person.name} person={person} />
+          )}
     </div>
   )
-
 }
 
 export default App
