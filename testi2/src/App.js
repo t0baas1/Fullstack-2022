15451284d45d7248ca9e15f6974ceaa489939d48
Topi Kanninen <topi.kanninen@sci.fi>
2022-03-19 13:58:41 +0200

    osa 2 alottelua

--- testi2/src/App.js
+++ testi2/src/App.js
@@ -1,16 +1,49 @@
+import { useState } from 'react'
 import Note from './components/Note'
 
 
+const App = (props) => {
+  const [notes, setNotes] = useState(props.notes)
+  const [newNote, setNewNote] = useState('')
+  const [showAll, setShowAll] = useState(true)
 
-const App = ({ notes }) => {
+  const handleNoteChange = (event) => {
+    console.log(event.target.value)
+    setNewNote(event.target.value)
+  }
+
+  const notesToShow = showAll
+    ? notes
+    : notes.filter(note => note.important === true)
+
+  const addNote = (event) => {
+    event.preventDefault()
+    const noteObject = {
+      content: newNote,
+      date: new Date().toISOString(),
+      important: Math.random() > 0.5,
+      id: notes.length + 1,
+    }
+  
+    setNotes(notes.concat(noteObject))
+    setNewNote('')
+  }
+
   return (
     <div>
       <h1>Notes</h1>
       <ul>
-        {notes.map(note =>
-        <Note key={note.id} note={note} />
+        {notesToShow.map(note =>
+          <Note key={note.id} note={note} />
         )}
       </ul>
+      <form onSubmit={addNote}>
+        <input
+          value={newNote}
+          onChange={handleNoteChange}
+        />
+        <button type="submit">save</button>
+      </form>
     </div>
   )
 }
