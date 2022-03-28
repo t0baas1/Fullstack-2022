7e96704348e0985c405ed3c7648f4cacb192da8b
Topi Kanninen <topi.kanninen@sci.fi>
2022-03-28 20:04:10 +0300

    part 2 jatkuu

--- osa2/puhelinluettelo/src/App.js
+++ osa2/puhelinluettelo/src/App.js
@@ -1,10 +1,20 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
+import axios from 'axios'
 
 const App = () => {
   const [persons, setPersons] = useState([]) 
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
 
+  useEffect(() => {
+    axios
+      .get('http://localhost:3001/persons')
+      .then(response => {
+        setPersons(response.data)
+      })
+  }, [])
+
+  console.log(persons)
 
   const handleNameChange = (event) => {
     setNewName(event.target.value)
