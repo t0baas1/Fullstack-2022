8a872f8906f0152ff41b548ffc47d0b6e6438b35
Topi Kanninen <topi.kanninen@sci.fi>
2022-04-01 22:40:58 +0300

    osa3 build-vaihe

--- testi2/src/services/notes.js
+++ testi2/src/services/notes.js
@@ -1,5 +1,5 @@
 import axios from 'axios'
-const baseUrl = 'https://glacial-brook-37170.herokuapp.com/api/notes'
+const baseUrl = '/api/notes'
 
 const getAll = () => {
     const request = axios.get(baseUrl)
