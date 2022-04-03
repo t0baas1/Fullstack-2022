6b516c4cce5128d6412ab0a7bb6b2cde0642f661
Topi Kanninen <topi.kanninen@sci.fi>
2022-04-03 18:28:59 +0300

    osa3 4

--- backend/index.js
+++ backend/index.js
@@ -24,7 +24,7 @@ app.get('/', (req, res) => {
     res.send('<h1>Hello World</h1>')
 })
 
-app.post('/api/notes', (request, response) => {
+app.post('/api/notes', (request, response, next) => {
     const body = request.body
 
     if (body.content === undefined) {
@@ -39,7 +39,8 @@ app.post('/api/notes', (request, response) => {
 
     note.save().then(savedNote => {
         response.json(savedNote)
-    })   
+    })
+    .catch(error => next(error))
 })
 
 app.get('/api/notes', (request, response) => {
@@ -69,14 +70,13 @@ app.get('/api/notes/:id', (request, response, next) => {
 })
 
 app.put('/api/notes/:id', (request, response, next) => {
-    const body = request.body
+    const { content, important } = request.body
 
-    const note = {
-        content: body.content,
-        important: body.important,
-    }
-
-    Note.findByIdAndUpdate(request.params.id, note, { new: true})
+    Note.findByIdAndUpdate(
+        request.params.id,
+        { content, important },
+        { new: true, runValidators: true, context: 'query'}
+        )
         .then(updatedNote => {
             response.json(updatedNote)
         })
@@ -94,6 +94,8 @@ const errorHandler = (error, request, response, next) => {
     
     if (error.name === 'CastError'){
         return response.status(400).send({ error: 'malformatted id'})
+    } else if (error.name === 'ValidationError') {
+        return response.status(400).json({ error: error.message })
     }
 
     next(error)
