fbebe416144ff9adef88e27efb538132c8118503
Topi Kanninen <topi.kanninen@sci.fi>
2022-04-03 16:06:58 +0300

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,7 @@
-require('dotenv').config()
 const express = require('express')
 const app = express()
 const cors = require('cors')
+require('dotenv').config()
 const Note = require('./models/note')
 const { response } = require('express')
 
